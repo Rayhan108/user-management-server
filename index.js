88554c4dc4970d5b11651e16c1ac5f2391f643a4
Rayhan Shorker <raihanshorker56@gmail.com>
2023-06-24 15:39:00 +0600

    create update and get single user api

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
-app.use(express.json())
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njyz70v.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -22,58 +22,67 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-})
+});
 
 async function run() {
   try {
-    const usersCollection = client.db('userManagement').collection('users')
-// User add api
+    const usersCollection = client.db("userManagement").collection("users");
+    // User add api
     app.post("/addUsers", async (req, res) => {
-        const newUser = req.body;
-  
-        const result = await usersCollection.insertOne(newUser);
-        res.send(result);
-      });
+      const newUser = req.body;
+
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
     //   get all users
-app.get('/users',async(req,res)=>{
-    const result = await usersCollection.find().toArray()
-    res.send(result);
-})
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    // get single users using params
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.findOne(query);
 
+      res.send(result);
+    });
+    // update users info
+    app.patch("/updateUser/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
 
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
 
+      const updateDoc = {
+        $set: {
+          name: updateUser.name,
+          email: updateUser.email,
+          phoneNumber: updateUser.phoneNumber,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc, option);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('User Management Server is running..')
-})
+app.get("/", (req, res) => {
+  res.send("User Management Server is running..");
+});
 
 app.listen(port, () => {
-  console.log(`User Management server is running on port ${port}`)
-})
+  console.log(`User Management server is running on port ${port}`);
+});
